95312861b9a52ac23a52604d0e217c9e356b8536
Laura Berge <lauraeberge@gmail.com>
2020-01-21 11:45:19 -0500

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state = {
+    text: ""
+  }
+
+  handleOnChange = ({target}) => {
+    this.setState({
+      text: target.value
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+
+    this.setState({
+      text: ""
+    })
+  }
+
   render() {
     return (
-      <div>
-        Restaurant Input
-      </div>
+      <form onSubmit={this.handleOnSubmit}>
+        <input type="text" onChange={this.handleOnChange} value={this.state.text}/>
+        <input type="submit"/>
+      </form>
     );
   }
 };
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,12 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const relevantReviews = this.props.reviews.filter(r => r.restaurantId === this.props.restaurantId)
     return (
       <ul>
-        Reviews
+        {relevantReviews.map(r => {
+          return <Review review={r} key={r.id} delete={this.props.delete}/>
+        })}
       </ul>
     );
   }
